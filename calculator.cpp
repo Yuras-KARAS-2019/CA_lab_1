2775042135e380a5731244f0e7b4e07bbefcb9ea
Yura Kubyshka <kubishka.yur@gmail.com>
2021-11-09 12:16:38 +0200

    Add new comments for your functions

--- calculator.cpp
+++ calculator.cpp
@@ -9,11 +9,20 @@ int Calculator::Add (double a, double b)
 //>>>>>>> 4ad40a3(fix truncation error)
 }
 
+/// <summary>
+/// Execute operation of subtraction
+/// </summary>
+/// <param name="a"></param>
+/// <param name="b"></param>
+/// <returns>int value ( a - b )</returns>
 int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
 }
 
+/// This function should 
+/// execute operation of multiplication;
+/// wada
 int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
